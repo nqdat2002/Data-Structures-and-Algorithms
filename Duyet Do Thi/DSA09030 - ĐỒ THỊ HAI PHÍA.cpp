d649b0f75d549178d72fbfeb24a1e09c238bb4d5
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-11-08 10:27:37 +0700

    Update DSA09030 - ĐỒ THỊ HAI PHÍA.cpp

--- Duyet Do Thi/DSA09030 - ĐỒ THỊ HAI PHÍA.cpp
+++ Duyet Do Thi/DSA09030 - ĐỒ THỊ HAI PHÍA.cpp
@@ -1,94 +1,47 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double ld;
-typedef pair<int, int> p;
-typedef vector<vector<int> > vt;
-typedef vector<pair<int, int> > vp;
-
-const ll mod = 1e9 + 7;
-const int oo = 1e6 + 7;
-
-#define f first
-#define s second
-#define pb push_back
-#define ep emplace_back
-#define sz(a) (int) a.size()
-#define ms(s, n) memset(s, n, sizeof(s))
-#define present(t, x) (t.find(x) != t.end())
-#define all(a) (a.begin(), a.end())
-#define For(i, l, r) for (int i = l; i <= r; i++)
-#define Fod(i, r, l) for (int i = r; i >= l; i--)
-#define fillchar(a, x) memset(a, x, sizeof (a))
-#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-void FileIO(){
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-}
-
-// Author: Nguyen Quoc Dat
-// ID: B20DCPT053
-// Organization: PTIT
-// Date: 07/03/22 10:30
-// Program: Do thi 2 phia
-// -------------------  Your code STARTS here !!!---------------------------
-
-bool visited[1005];
-vector<int> adj[1005];
-int color[1005];
-int n, m;
-bool check;
-
-void DFS(int u, int t){
-    visited[u] = true;
-    for(auto v: adj[u]){
-        if(!visited[v]){
-            if(color[v] == 0)
-                color[v] = 3 - color[u];
-            DFS(v, u);
-            visited[v] = true;
+vector<int> a[1005];
+int c[1005], n, m;
+bool chuaxet[1005], check;
+void dfs(int u, int t) {
+    chuaxet[u] = false;
+    for (int i = 0; i < a[u].size(); i++) {
+        int v = a[u][i];
+        if (chuaxet[v]) {
+            if (c[v] == 0)
+                c[v] = 3 - c[u];
+            dfs(v, u);
         }
     }
 }
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int t; cin >> t;
-    while(t--){
-        for(int i = 0; i <= 1005; i ++) adj[i].clear();
+int main() {
+    int t, i, j, u, v;
+    cin >> t;
+    while (t--) {
+        for (i = 1; i <= 1005; i++)
+            a[i].clear();
+        memset(c, 0, sizeof(c));
+        memset(chuaxet, true, sizeof(chuaxet));
         cin >> n >> m;
-        for(int i = 1; i <= m; i ++){
-            int x, y; cin >> x >> y;
-            adj[x].push_back(y);
-            adj[y].push_back(x);
+        for (i = 1; i <= m; i++) {
+            cin >> u >> v;
+            a[u].push_back(v);
+            a[v].push_back(u);
         }
-
-        memset(visited, false, sizeof(visited));
-        memset(color, 0, sizeof(color));
-        for(int u = 1; u <= n; ++u){
-            if(color[u] == 0){
-                color[u] = 1;
-                DFS(u, 0);
+        for (i = 1; i <= n; i++) {
+            if (c[i] == 0) {
+                c[i] = 1;
+                dfs(i, 0);
             }
         }
-        check = false;
-        for(int u = 1; u <= n; ++u){
-            for(auto v: adj[u]){
-                if(color[u] == color[v]){
+        check = true;
+        for (i = 1; i <= n; i++) {
+            for (j = 0; j < a[i].size(); j++) {
+                if (c[i] == c[a[i][j]])
                     check = false;
-                    break;
-                }
-                if(!check) break;
-            }   
+            }
         }
-        cout << (check == true ? "YES" : "NO") << endl;
+        cout << ((check) ? "YES" : "NO") << endl;
     }
     return 0;
 }
